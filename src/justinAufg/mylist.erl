a593f4456ce35a256dcc866de9831b133f11ecd1
act752 <justin.hoffmann@haw-hamburg.de>
2020-10-26 14:44:31 +0100

    comments

--- src/justinAufg/mylist.erl
+++ src/justinAufg/mylist.erl
@@ -4,39 +4,48 @@
 %% API Functions
 -export([member/2, substitute/4, compress/1]).
 
+%% Internal functions
+
+%% member(E, L) überprüft ob Element E in Liste L min. einmal vorkommt
 member(_, []) -> false;
 member(Element, [Element|_]) -> true;
 member(Element, [_|Tail]) -> member(Element, Tail).
 
-%%
-
+%% substitute(E, S, L, P) ersetzt Element E in Liste E mit dem Substitut S
+%% relativ zur Position P
 substitute(Element, Substitute, List, a) -> subAll(Element, Substitute, List);  
 substitute(Element, Substitute, List, e) -> subFirst(Element, Substitute, List);
 substitute(Element, Substitute, List, l) -> subLast(Element, Substitute, List). 
 
+%% substitute(E, S, L) ersetzt alle Elemente E in Liste E mit dem Substitut S
 subAll(Element, Substitute, [Element|Tail]) -> [Substitute|subAll(Element, Substitute, Tail)];
 subAll(Element, Substitute, [Head|Tail]) -> [Head|subAll(Element, Substitute, Tail)];
 subAll(_, _, []) -> [].  
 
+%% substitute(E, S, L) ersetzt das erste Element E in Liste E mit dem Substitut S
 subFirst(Element, Substitute, [Element|Tail]) -> [Substitute|Tail];
 subFirst(Element, Substitute, [_|Tail]) -> subFirst(Element, Substitute, Tail);
 subFirst(_, _, []) -> [].
 
+%% substitute(E, S, L) ersetzt das letzte Element E in Liste E mit dem Substitut S
 subLast(Element, Substitute, List) -> invert(subFirst(Element, Substitute, invert(List))).
 
+%% invert(L) invertiert die Liste L
 invert([Head|Tail]) -> invert(Tail) ++ [Head];
 invert([]) -> [].
 
-%%
-
+%% compress(L) gibt eine Liste aller einzigartigen Elemente der Liste L und deren
+%% respektive Vorkommens-Zahl zurück
 compress([Head|Tail]) ->
     [{Head, count(Head, [Head|Tail])}] ++ compress(reduce(Head, Tail));
 compress([]) -> [].
 
+%% count(E, L) zählt die Vorkommnisse von Element E in Liste L
 count(_, []) -> 0;
 count(Element, [Element|Tail]) -> count(Element, Tail) + 1;
 count(Element, [_|Tail]) -> count(Element, Tail).
 
+%% reduce(E, L) entfernt alle Vorkommnisse von E aus der Liste L
 reduce(Element, [Element|Tail]) -> reduce(Element, Tail);
 reduce(Element, [Head|Tail]) -> [Head] ++ reduce(Element, Tail);
 reduce(_, []) -> [].
--- src/justinAufg/tut.erl
+++ src/justinAufg/tut.erl
@@ -1,15 +1,26 @@
 %% @author justinh
 -module(tut).
--import(string, [len/1, concat/2, chr/2, substr/3,
-                to_lower/1, to_upper/1]).
-
+-import(string, [concat/2]).
 %% API functions
--export([main/0]).
+-export([minimum/1, maximum/1, minimum_maximum/1]).
 
 %% Internal functions
-main() -> 
-    compare(4, 4.0).
 
-compare(A, B) ->
-    A == B.
-    
+%% minimum(L) ermittelt das kleinste Element der Liste L
+minimum([Head|Tail]) -> minimum(Head, [Head|Tail]).
+minimum(Element, [Head|Tail]) when Head < Element -> minimum(Head, Tail);
+minimum(Element, [_|Tail]) -> minimum(Element, Tail);
+minimum(Element, []) -> Element.
+
+%% maximum(L) ermittelt das kleinste Element der Liste L
+maximum([Head|Tail]) -> maximum(Head, [Head|Tail]).
+maximum(Element, [Head|Tail]) when Head > Element -> maximum(Head, Tail);
+maximum(Element, [_|Tail]) -> maximum(Element, Tail);
+maximum(Element, []) -> Element.
+
+%% minimum_maximum(L) gibt ein Tupel mit Maximum und Minimum der Liste L aus
+minimum_maximum(List) -> {minimum(List), maximum(List)}.
+
+
+
+
