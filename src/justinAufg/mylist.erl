1bb29d344906dce76ffb1a40aaeacdc3ffe6a279
act752 <justin.hoffmann@haw-hamburg.de>
2020-10-27 15:59:29 +0100

    del_element und diffList

--- src/justinAufg/mylist.erl
+++ src/justinAufg/mylist.erl
@@ -2,7 +2,8 @@
 -module(mylist).
 
 %% API Functions
--export([member/2, substitute/4, compress/1]).
+-export([member/2, substitute/4, compress/1, diffList/2, del_element/3,
+        eo_count/1]).
 
 %% Internal functions
 
@@ -13,24 +14,11 @@ member(Element, [_|Tail]) -> member(Element, Tail).
 
 %% substitute(E, S, L, P) ersetzt Element E in Liste E mit dem Substitut S
 %% relativ zur Position P
-substitute(Element, Substitute, List, a) -> subAll(Element, Substitute, List);  
-substitute(Element, Substitute, List, e) -> subFirst(Element, Substitute, List);
-substitute(Element, Substitute, List, l) -> subLast(Element, Substitute, List). 
-
-%% subAll(E, S, L) ersetzt alle Elemente E in Liste E mit dem Substitut S
-subAll(_, _, []) -> [];
-subAll(Element, Substitute, [Element|Tail]) -> [Substitute|subAll(Element, Substitute, Tail)];
-subAll(Element, Substitute, [Head|Tail]) -> [Head|subAll(Element, Substitute, Tail)].
-
-
-%% subFirst(E, S, L) ersetzt das erste Element E in Liste E mit dem Substitut S
-subFirst(_, _, []) -> [];
-subFirst(Element, Substitute, [Element|Tail]) -> [Substitute|Tail];
-subFirst(Element, Substitute, [_|Tail]) -> subFirst(Element, Substitute, Tail).
-
-
-%% subsLast(E, S, L) ersetzt das letzte Element E in Liste E mit dem Substitut S
-subLast(Element, Substitute, List) -> invert(subFirst(Element, Substitute, invert(List))).
+substitute(_, _, [], _) -> [];
+substitute(Element, Substitute, [Element|Tail], a) -> [Substitute|substitute(Element, Substitute, Tail, a)];
+substitute(Element, Substitute, [Element|Tail], e) -> [Substitute|Tail];
+substitute(Element, Substitute, List, l) -> invert(substitute(Element, Substitute, invert(List), e));
+substitute(Element, Substitute, [Head|Tail], Position) -> [Head|substitute(Element, Substitute, Tail, Position)].
 
 %% invert(L) invertiert die Liste L
 invert([]) -> [];
@@ -48,17 +36,31 @@ count(_, []) -> 0;
 count(Element, [Element|Tail]) -> count(Element, Tail) + 1;
 count(Element, [_|Tail]) -> count(Element, Tail).
 
-%% =================================================================
-%% reduce(E, L) entfernt alle Vorkommnisse von E aus der Liste L
-%% reduce(Element, [Element|Tail]) -> reduce(Element, Tail);
-%% reduce(Element, [Head|Tail]) -> [Head] ++ reduce(Element, Tail);
-%% reduce(_, []) -> [].
-%% =================================================================
-
 %% reduce(E, L) via List Comprehension
 reduce(Element, List) -> 
     [ListElement || ListElement <- List, ListElement =/= Element].
 
+
+
+%% diffList(L1, L2) bildet die Listendifferenz aus L1 und L2
+diffList(List1, List2) ->
+    List1 -- List2.
+
+
+
+%% del_element(P, E, L) entfernt Element E aus Liste L relativ zur Position P
+del_element(_, _, []) -> [];
+del_element(a, Element, [Element|Tail]) -> del_element(a, Element, Tail);
+del_element(e, Element, [Element|Tail]) -> Tail;
+del_element(l, Element, List) -> invert(del_element(e, Element, invert(List)));
+del_element(Position, Element, [Head|Tail]) -> [Head|del_element(Position, Element, Tail)].
+
+
+
+
+eo_count(List) -> 
+    List = true.
+    
 
 
 
