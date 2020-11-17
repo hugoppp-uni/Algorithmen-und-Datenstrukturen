7eb88c2b90c2b06e399076ea9538b9c5c7742436
Hugo Protsch <hugo.protsch@haw-hamburg.de>
2020-11-17 21:09:05 +0100

    Add sortAnalyse.erl

---
+++ src/prak2/sortAnalyse.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author Justin Hoffmann
+%%% @author Hugo Protsch
+%%%-------------------------------------------------------------------
+-module(sortAnalyse).
+-author("Justin Hoffmann").
+-author("Hugo Protsch").
+
+%% API
+-export([]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+time() -> todo.
+
