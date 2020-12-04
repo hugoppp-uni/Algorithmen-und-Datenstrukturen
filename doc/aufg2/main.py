1a7a5d628e30ba6bbc57dcc637df18b4f74161da
Hugo Protsch <hugo.protsch@haw-hamburg.de>
2020-12-04 17:05:53 +0100

    Add qsort measurements and graph (first run)

--- doc/aufg2/main.py
+++ doc/aufg2/main.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.axes._axes as axes
 import csv
 
 
-def generate_graph(pathArray, filename, legend, clipBegin=0.0, clipEnd=0.0):
+def generate_graph(titl, xla, yla, d, pathArray, filename, legend, clipBegin=0.0, clipEnd=0.0):
     xArr = []
     yArr = []
+    fig = plt.figure()
+    ax1 = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+    assert isinstance(ax1, axes.Axes)
     for path in pathArray:
         x = []
         y = []
@@ -19,20 +23,32 @@ def generate_graph(pathArray, filename, legend, clipBegin=0.0, clipEnd=0.0):
             yArr.append(y)
 
     for i in range(0, len(xArr)):
-        plt.plot(np.array(xArr[i], int), np.array(yArr[i], int), '.')
-    plt.axhline(0, color='lightgrey', lw=1)
-    plt.axvline(0, color='lightgrey', lw=1)
-    plt.legend(legend, )
-    plt.autoscale(enable=True, axis='both')
-    # plt.savefig('out/' + filename)
-    plt.show()
-    plt.clf()
+        ax1.plot(np.array(xArr[i], int), np.array(yArr[i], int), '--.', linewidth=1)
+    # ax1.axhline(0, color='lightgrey', lw=1)
+    # plt.axvline(0, color='lightgrey', lw=1)
+    ax1.legend(legend)
+    ax1.autoscale(enable=True, axis='both')
+    plt.title(titl)
+    plt.xlabel(xla)
+    plt.ylabel(yla)
+    plt.figtext(0.5, 0.06, d, ha="center",  # fontsize=12,
+                bbox={"facecolor": "lightgrey", "alpha": 0.5, "pad": 5})
+    # fig.set_size_inches(7, 8, forward=True)
+    fig.savefig('out/' + filename, bbox_inches='tight')
+    # fig.show()
+    # ax1.clf()
 
 
-plt.figure()
-plt.title('qsort list comprh test')
-plt.xlabel('Elements')
-plt.ylabel('Time / ms')
+t = 'Pivot Methods Comparison - First Implementation'
+xl = 'Elements'
+yl = 'Time / ms'
+desc = "Average over 20 runs, random numbers, 'left' pivot method"
 
-generate_graph(['data/qsort/randLeft20_ListComp_1.csv', 'data/qsort/randLeft20_1.csv'],
-              'plot1', ['list comprehension', 'own implementation'])
+generate_graph(t, xl, yl, desc,
+               ['data/qsort/firstRun/left.csv',
+                'data/qsort/firstRun/right.csv',
+                'data/qsort/firstRun/middle.csv',
+                'data/qsort/firstRun/median.csv',
+                'data/qsort/firstRun/random.csv'],
+               'pivotMethods_firstImplementation.pdf',
+               ['left', 'right', 'middle', 'median', 'random'])
