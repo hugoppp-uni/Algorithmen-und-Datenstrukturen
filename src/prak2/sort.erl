b46b19d32a6fa2845940b805ee1d2de39ae6c997
act752 <justin.hoffmann@haw-hamburg.de>
2020-12-05 14:20:11 +0100

    text hsort ueberarbeitet

--- src/prak2/sort.erl
+++ src/prak2/sort.erl
@@ -151,6 +151,24 @@ removeSwappedRoot(Heap) -> ok.
 
 heapify(Heap) -> ok.
 
+% Kodierung des Feldes: Nachfolger von Position i ist 2*i links und 2*i+1 rechts
+% berechnet den Pfad zur ersten leeren Position
+% l steht fuer links, r fuer rechts
+% Beispiel: sort:calcPath(1). --> []
+% 		sort:calcPath(2). --> [l]
+% 		sort:calcPath(3). --> [r]
+% 		sort:calcPath(4). --> [l,l]
+% 		sort:calcPath(5). --> [l,r]
+% 		sort:calcPath(6). --> [r,l] 
+% 		sort:calcPath(7). --> [r,r] 
+calcPath(Number) -> calcPath(Number,[]).
+% aktuelle Position ist Wurzel
+calcPath(1,Accu) -> Accu;
+% aktuelle Position ist gerade
+calcPath(Number,Accu) when Number rem 2 =:= 0 -> calcPath(Number div 2,[l|Accu]);
+% aktuelle Position ist ungerade
+calcPath(Number,Accu) when Number rem 2 =/= 0 -> calcPath((Number-1) div 2,[r|Accu]).
+
 
 
 
